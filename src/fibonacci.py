1853254bb9d3dfd382313f8367414c6e4b8be48a
The Sam <muer.powell@gmail.com>
2024-05-01 19:48:05 +1200

    Add performance testing

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -18,10 +18,31 @@ def fibonacci(num: int) -> int:
     Returns:
         The result of f(n) = f(n - 1) + f(n - 2)
     """
-    if num ==0:
+    if num == 0:
         return 0
-    
+
     if num <= 2:
         return 1
-    
+
     return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+def fib_no_cache(num: int) -> int:
+    """
+    Calculate result of the fibonacci sequence:
+    f 0, 1, 2, 3, 4, 5,...,10
+    v 0, 1, 1, 2, 3, 5,...,55
+
+    Arg:
+        num(int): The number to calculate.
+
+    Returns:
+        The result of f(n) = f(n - 1) + f(n - 2)
+    """
+    if num == 0:
+        return 0
+
+    if num <= 2:
+        return 1
+
+    return fib_no_cache(num - 1) + fib_no_cache(num - 2)
---
+++ tests/performance.py
@@ -0,0 +1,62 @@
+import logging
+import random
+import time
+from typing import Callable
+from src.fibonacci import fibonacci, fib_no_cache
+
+# Constants
+SEED = 42
+FIBS_TO_MAKE = 11
+RAND_RANGE = (0, 40)
+
+
+def configuration():
+    """Runs performance configurations"""
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(levelname)s: %(message)s",
+    )
+    random.seed(SEED)
+
+
+def time_it(func: Callable[[int], int]) -> float:
+    """Times the passed in fib fuinction."""
+    logging.info("Running %s", func.__name__)
+    start: float = time.time()
+    for i in range(FIBS_TO_MAKE):
+        value: int = random.randint(*RAND_RANGE)
+        res: int = func(value)
+        logging.debug(f"{i:2} - f({value:2}) = {res:14}")
+    finish: float = time.time()
+    elapsed: float = finish - start
+    return elapsed
+
+
+def call_one() -> float:
+    """Non-caching call"""
+    return time_it(fib_no_cache)
+
+
+def call_two() -> float:
+    """First caching call"""
+    fibonacci.cache_clear()
+    return time_it(fibonacci)
+
+
+def call_three() -> float:
+    """Second caching call"""
+    return time_it(fibonacci)
+
+
+def main() -> None:
+    """Call the things"""
+
+    print("Cached time: ", call_one())
+    print("No cache time (1): ", call_two())
+    print(fibonacci.cache_info())
+    print("No cache time (2): ", call_three())
+    print(fibonacci.cache_info())
+
+
+if __name__ == "__main__":
+    main()
